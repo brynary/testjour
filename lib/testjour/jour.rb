332aa028d1743133f0d71befe5faf90eb404905f
Bryan Helmkamp <bryan@brynary.com>
2008-09-24 00:32:24 -0400

    Introducing Testjour::Jour::Server object for server list results

--- lib/testjour/jour.rb
+++ lib/testjour/jour.rb
@@ -5,23 +5,41 @@ require "set"
 Thread.abort_on_exception = true
 
 module Testjour
-  Server  = Struct.new(:name, :host, :port)
   SERVICE = "_testjour._tcp"  
   
   class Jour
+    
+    class Server
+      attr_reader :name, :host, :port
+      
+      def initialize(name, host, port)
+        @name = name
+        @host = host
+        @port = port
+      end
+      
+      def ==(other)
+        other.class == self.class && other.uri == self.uri
+      end
+      
+      def uri
+        "druby://" + @host.gsub(/\.$/, "") + ":" + @port.to_s
+      end
+    end
     
     def self.list
       hosts = []
 
       service = DNSSD.browse(SERVICE) do |reply|
         DNSSD.resolve(reply.name, reply.type, reply.domain) do |rr|
-          hosts << Server.new(reply.name, rr.target, rr.port)
+          server = Server.new(reply.name, rr.target, rr.port)
+          hosts << server unless hosts.any? { |h| h == server }
         end
       end
 
       sleep 3
       service.stop
-      hosts.uniq
+      return hosts
     end
     
     def self.serve(port)
