98f81d2293c4df3e7ef941ba930431104aea9966
Bryan Helmkamp <bryan@brynary.com>
2008-12-09 19:37:09 -0500

    Refactoring Rsync

--- lib/testjour/rsync.rb
+++ lib/testjour/rsync.rb
@@ -8,23 +8,45 @@ module Testjour
   class Rsync
     
     def self.copy_to_current_directory_from(source_uri)
+      new(source_uri).copy_with_retry
+    end
+    
+    def initialize(source_uri)
+      @source_uri = source_uri
+    end
+
+    def copy_with_retry
       retryable :tries => 2, :on => RsyncFailed do
-        destination_dir = File.expand_path(".")
-        uri = URI.parse(source_uri)
-      
-        command = "rsync -az --delete --exclude=.git --exclude=*.log --exclude=*.pid #{uri.user}@#{uri.host}:#{uri.path}/ #{destination_dir}"
-      
         Testjour.logger.info "Rsyncing: #{command}"
-        start_time = Time.now
-        successful = system command
-      
-        if successful
-          time = Time.now - start_time
-          Testjour.logger.debug("Rsync finished in %.2fs" % time)
-        else
-          raise RsyncFailed.new
-        end
+        copy
+        Testjour.logger.debug("Rsync finished in %.2fs" % elapsed_time)
+        raise RsyncFailed.new unless successful?
       end
+    end
+    
+    def copy
+      @start_time = Time.now
+      @successful = system(command)
+    end
+    
+    def elapsed_time
+      Time.now - @start_time
+    end
+    
+    def successful?
+      @successful
+    end
+    
+    def command
+      "rsync -az --delete --exclude=.git --exclude=*.log --exclude=*.pid #{uri.user}@#{uri.host}:#{uri.path}/ #{destination_dir}"
+    end
+    
+    def destination_dir
+      File.expand_path(".")
+    end
+      
+    def uri
+      URI.parse(@source_uri)
     end
     
   end
