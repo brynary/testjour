cc92ba32b82293135e9da57db0157fe17149ae68
Bryan Helmkamp <bryan@brynary.com>
2009-02-03 02:19:59 -0500

    Queue feature files before spawning local:run

--- lib/testjour/commands/run.rb
+++ lib/testjour/commands/run.rb
@@ -1,6 +1,7 @@
 require "testjour/commands/command"
 require "testjour/http_queue"
 require "systemu"
+require "net/http"
 
 module Testjour
 module Commands
@@ -22,11 +23,30 @@ module Commands
       
       HttpQueue.wait_for_service
       
+      require 'cucumber/cli/main'
+      configuration.load_language
+      
+      HttpQueue.with_net_http do |http|
+        configuration.feature_files.each do |feature_file|
+          post = Net::HTTP::Post.new("/")
+          post.form_data = {"feature_file" => feature_file}
+          http.request(post)
+        end
+      end
+        
       status, stdout, stderr = systemu(cmd)
       
       @out_stream.write stdout
       @err_stream.write stderr
       status.exitstatus
+    end
+    
+    def configuration
+      return @configuration if @configuration
+      
+      @configuration = Cucumber::Cli::Configuration.new(StringIO.new, StringIO.new)
+      @configuration.parse!(@args)
+      @configuration
     end
     
   end
