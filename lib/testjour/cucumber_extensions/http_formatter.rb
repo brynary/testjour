96d10a1154fc2bf4ce93b24418b5842434e0c9da
Bryan Helmkamp <bryan@brynary.com>
2009-02-03 03:08:16 -0500

    Post step results to the results queue

--- lib/testjour/cucumber_extensions/http_formatter.rb
+++ lib/testjour/cucumber_extensions/http_formatter.rb
@@ -10,27 +10,13 @@ module Testjour
         @io = io
         @options = options
       end
-
-      def visit_features(features)
-        with_color do
-          super
-          @io.puts
-          @io.puts
-          print_summary(features)
-        end
-      end
-
+      
       def visit_multiline_arg(multiline_arg, status)
         @multiline_arg = true
         super
         @multiline_arg = false
       end
-
-      def visit_feature_element(feature_element)
-        progress(:undefined) if feature_element.undefined?
-        super
-      end
-
+      
       def visit_step_name(keyword, step_name, status, step_definition, source_indent)
         progress(status) unless status == :outline
       end
@@ -41,26 +27,19 @@ module Testjour
       
       private
 
-      def print_summary(features)
-        print_undefined_scenarios(features)
-        print_steps(features, :pending)
-        print_steps(features, :failed)
-        print_counts(features)
-        print_snippets(features, @options)
-      end
-
       CHARS = {
         :passed    => '.',
         :failed    => 'F',
-        :undefined => 'U',
         :pending   => 'P',
         :skipped   => 'S'
       }
 
       def progress(status)
-        char = CHARS[status]
-        @io.print(format_string(char, status))
-        @io.flush
+        HttpQueue.with_net_http do |http|
+          post = Net::HTTP::Post.new("/results")
+          post.form_data = { "data" => CHARS[status] }
+          http.request(post)
+        end
       end
       
     end
