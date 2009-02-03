4f9e04d9a0a537c6a346fad122ee2568f942b4e3
Bryan Helmkamp <bryan@brynary.com>
2009-02-02 19:37:41 -0500

    Move HttpQueue definition to lib/testjour

---
+++ lib/testjour/http_queue.rb
@@ -0,0 +1,77 @@
+class HttpQueue
+  
+  def self.queue
+    @queue ||= Queue.new
+  end
+
+  def self.call(env)
+    new(env).call
+  end
+  
+  def initialize(env)
+    @request = Rack::Request.new(env)
+  end
+  
+  def call
+    if request.post?
+      handle_post
+    else
+      handle_get
+    end
+  end
+  
+protected
+  
+  def request
+    @request
+  end
+  
+  def handle_get
+    case request.path_info
+    when "/reset" then reset
+    when "/"      then pop
+    else error
+    end
+  end
+  
+  def handle_post
+    case request.path_info
+    when "/"  then push
+    else error
+    end
+  end
+  
+  def reset
+    self.class.queue.clear
+    ok
+  end
+  
+  def pop
+    feature = self.class.queue.pop(true)
+    [200, { "Content-Type" => "text/plain" }, feature]
+  rescue ThreadError => ex
+    if ex.message =~ /queue empty/
+      missing
+    else
+      raise
+    end
+  end
+  
+  def push
+    self.class.queue.push(request.POST["feature_file"])
+    ok
+  end
+  
+  def ok
+    [200, { "Content-Type" => "text/plain" }, "OK"]
+  end
+  
+  def missing
+    [404, { "Content-Type" => "text/plain" }, "Not Found"]
+  end
+  
+  def error
+    [500, { "Content-Type" => "text/plain" }, "Server error"]
+  end
+  
+end
