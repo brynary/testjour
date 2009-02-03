8734c175f779211727739c4edf0b145a62367338
Bryan Helmkamp <bryan@brynary.com>
2009-02-03 01:03:36 -0500

    Refactoring Run command

--- lib/testjour/commands/run.rb
+++ lib/testjour/commands/run.rb
@@ -11,36 +11,60 @@ module Commands
       end
       
       require 'cucumber/cli/main'
-      step_mother = Cucumber::Cli::Main.instance_variable_get("@step_mother")
       
-      configuration = Cucumber::Cli::Configuration.new(StringIO.new, StringIO.new)
-      configuration.parse!(@args)
       configuration.load_language
       step_mother.options = configuration.options
 
-      configuration.files_to_require.each do |lib|
-        require lib
-      end
-    
-      features = Cucumber::Ast::Features.new(configuration.ast_filter)
-      parser = Cucumber::Parser::FeatureParser.new
-
-      configuration.feature_files.each do |f|
-        features.add_feature(parser.parse_file(f))
-      end
-
-      visitor = configuration.build_formatter_broadcaster(step_mother)
-      visitor.visit_features(features)
-    
-      failure = features.steps[:failed].any? || (configuration.strict? && features.steps[:undefined].length)
+      require_files
+      features = load_plain_text_features
+      visit_features(features)
       
-      if failure
+      if failed?(features)
         @out_stream.puts "Failed"
         1
       else
         @out_stream.puts "Passed"
         0
       end
+    end
+    
+    def visit_features(features)
+      visitor = configuration.build_formatter_broadcaster(step_mother)
+      visitor.visit_features(features)
+    end
+    
+    def configuration
+      return @configuration if @configuration
+      
+      @configuration = Cucumber::Cli::Configuration.new(StringIO.new, StringIO.new)
+      @configuration.parse!(@args)
+      @configuration
+    end
+    
+    def require_files
+      configuration.files_to_require.each do |lib|
+        require lib
+      end
+    end
+    
+    def load_plain_text_features
+      features = Cucumber::Ast::Features.new(configuration.ast_filter)
+      parser = Cucumber::Parser::FeatureParser.new
+
+      configuration.feature_files.each do |f|
+        features.add_feature(parser.parse_file(f))
+      end
+      
+      return features
+    end
+    
+    def failed?(features)
+      features.steps[:failed].any? ||
+      (configuration.strict? && features.steps[:undefined].length)
+    end
+    
+    def step_mother
+      Cucumber::Cli::Main.instance_variable_get("@step_mother")
     end
     
   end
