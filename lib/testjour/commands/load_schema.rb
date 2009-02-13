6538d240f965c112dcc4fa3c79e339f0d1cf4667
Bryan Helmkamp <bryan@brynary.com>
2009-02-13 10:29:41 -0500

    Log errors in schema:load

--- lib/testjour/commands/load_schema.rb
+++ lib/testjour/commands/load_schema.rb
@@ -12,6 +12,10 @@ module Commands
       mysql = MysqlDatabaseSetup.new(database_name)
       mysql.load_schema
       return 0
+    rescue Object => ex  
+      Testjour.logger.error "load:schema error: #{ex.message}"
+      Testjour.logger.error ex.backtrace.join("\n")
+      return 1
     end
     
   end
