512adec2229e4615c75f1833e89fb0cbffb2cb8d
Bryan Helmkamp <bryan@brynary.com>
2009-06-10 16:27:03 -0400

    Fix another place where parsing features can be nil

--- lib/testjour/commands/command.rb
+++ lib/testjour/commands/command.rb
@@ -2,45 +2,49 @@ module Testjour
 module Commands
 
   class Command
-    
+
     def initialize(args = [], out_stream = STDOUT, err_stream = STDERR)
       @options = {}
       @args = args
       @out_stream = out_stream
       @err_stream = err_stream
     end
-    
+
   protected
-    
+
     def configuration
       return @configuration if @configuration
       @configuration = Configuration.new(@args)
       @configuration
     end
-    
+
     def load_plain_text_features(files)
       features = Cucumber::Ast::Features.new
-      
+
       Array(files).each do |file|
-        features.add_feature(parser.parse_file(file, configuration.cucumber_configuration.options))
+        parsed_feature = parser.parse_file(file, configuration.cucumber_configuration.options)
+
+        if parsed_feature
+          features.add_feature(parsed_feature)
+        end
       end
-      
+
       return features
     end
-    
+
     def parser
       @parser ||= Cucumber::Parser::FeatureParser.new
     end
-    
+
     def step_mother
       Cucumber::Cli::Main.instance_variable_get("@step_mother")
     end
-    
+
     def testjour_path
       File.expand_path(File.dirname(__FILE__) + "/../../../bin/testjour")
     end
-    
+
   end
-  
+
 end
 end
