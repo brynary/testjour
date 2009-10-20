3288881492e23b09594c3aa7b54cd1bd563e8c37
Bryan Helmkamp <bryan@brynary.com>
2009-10-20 16:14:54 -0400

    Always remove Testjour options from ARGV

--- lib/testjour/cli.rb
+++ lib/testjour/cli.rb
@@ -2,40 +2,39 @@ require "testjour/commands"
 
 module Testjour
   class CLI
-    
+
     def self.execute(*args)
       new(*args).execute
     end
-    
+
     def initialize(args, out_stream = STDOUT, err_stream = STDERR)
       @args = args
       @out_stream = out_stream
       @err_stream = err_stream
     end
-    
-    def command_class(args)
-      if args.first == "--help"
-        @args_for_command = @args[1..-1]
+
+    def command_class
+      case @args.first
+      when "--help"
+        @args.shift
         Commands::Help
-      elsif args.first == "--version"
-        @args_for_command = @args[1..-1]
+      when "--version"
+        @args.shift
         Commands::Version
-      elsif args.first == "run:slave"
-        @args_for_command = @args[1..-1]
+      when "run:slave"
+        @args.shift
         Commands::RunSlave
-      elsif args.first == "run:remote"
-        @args_for_command = @args[1..-1]
+      when "run:remote"
+        @args.shift
         Commands::RunRemote
       else
-        @args_for_command = @args.dup
         Commands::Run
       end
     end
-    
+
     def execute
-      klass = command_class(@args)
-      klass.new(@args_for_command, @out_stream, @err_stream).execute || 0
+      command_class.new(@args, @out_stream, @err_stream).execute || 0
     end
-    
+
   end
 end
